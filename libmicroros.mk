13dd77ceae4d661ea8e5db1c7629d90a916deeb9
ksatoshi <b1020257@fun.ac.jp>
2025-02-01 15:36:08 +0900

    ertps, rmw_ertpsの参照先を変更

--- libmicroros.mk
+++ libmicroros.mk
@@ -49,8 +49,8 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 	rm -rf micro_ros_src; \
 	mkdir micro_ros_src; cd micro_ros_src; \
 	if [ "$(MIDDLEWARE)" = "embeddedrtps" ]; then \
-		git clone -b main https://github.com/micro-ROS/embeddedRTPS src/embeddedRTPS; \
-		git clone -b main https://github.com/micro-ROS/rmw_embeddedrtps src/rmw_embeddedrtps; \
+		git clone -b main https://github.com/oss-fun/embeddedRTPS.git src/embeddedRTPS; \
+		git clone -b main https://github.com/oss-fun/rmw_embeddedrtps.git src/rmw_embeddedrtps; \
 	else \
 		git clone -b ros2 https://github.com/eProsima/Micro-XRCE-DDS-Client src/Micro-XRCE-DDS-Client; \
 		git clone -b jazzy https://github.com/micro-ROS/rmw_microxrcedds src/rmw_microxrcedds; \
